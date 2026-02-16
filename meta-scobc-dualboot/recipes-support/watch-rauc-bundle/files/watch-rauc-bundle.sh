e79db809152de3a11eecdec3a655302b2cd00b22
KantaTamura <kanta@spacecubics.com>
2026-02-16 16:54:48 +0000

    dualboot: enable download-only updates on main and switch to golden after bundle download
    - Install rauc-hawkbit-updater on main image
    - Add watch-rauc-bundle systemd service to detect bundle.raucb download
    completion
      and trigger boot-to-golden for rollback/switch flow
    - Use rauc-hawkbit-updater v1.4 because v1.3 does not send "finished"
    after download
    
    Signed-off-by: KantaTamura <kanta@spacecubics.com>

---
+++ meta-scobc-dualboot/recipes-support/watch-rauc-bundle/files/watch-rauc-bundle.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+set -eu
+
+WATCH_DIR="/srv/data/artifacts"
+BUNDLE_NAME="bundle.raucb"
+BUNDLE_PATH="$WATCH_DIR/$BUNDLE_NAME"
+
+SCRIPT="/usr/sbin/boot-to-golden"
+
+[ -d "$WATCH_DIR" ] || exit 0
+
+while :; do
+	fname=$(/usr/bin/inotifywait -e close_write --format '%f' "$WATCH_DIR")
+	[ "$fname" = "$BUNDLE_NAME" ] || continue
+
+	if [ "$fname" = "$BUNDLE_NAME" ] && [ -f "$BUNDLE_PATH" ]; then
+		logger -t watch-rauc-bundle "Detected $BUNDLE_PATH; running boot-to-golden"
+		exec "$SCRIPT"
+	fi
+done
